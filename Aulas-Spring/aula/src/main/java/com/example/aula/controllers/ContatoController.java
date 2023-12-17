9f5e22ebda0cfabbcfa0a2fbb6b6a7922a215466
SrGalderius <lucas-galdino-rosa@hotmail.com>
2023-12-17 09:58:06 -0300

    Finalizando verbos de Requisição HTTP

--- Aulas-Spring/aula/src/main/java/com/example/aula/controllers/ContatoController.java
+++ Aulas-Spring/aula/src/main/java/com/example/aula/controllers/ContatoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +30,17 @@ public class ContatoController {
 	}
 
 	@PutMapping("/{idContato}")
-	public String alterar(@PathVariable("idContato") Long idContato, @RequestBody String contato) {
-		return contato;
+	public ResponseEntity<Object> alterar(@PathVariable("idContato") Long idContato,
+										  @RequestBody Contato contato) {
+		for (Contato ct : contatos) {
+			if (ct.getId().equals(idContato)) {
+				ct.setNome(contato.getNome());
+				ct.setEmail(contato.getEmail());
+				ct.setFone(contato.getFone());				
+				return ResponseEntity.status(HttpStatus.OK).body(ct);
+			}
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não Encontrado...");
 	}
 
 	@GetMapping
@@ -39,12 +49,23 @@ public class ContatoController {
 	}
 
 	@GetMapping("/{idContato}")
-	public ResponseEntity<Contato> consultar(@PathVariable("idContato") Long idContato) {
+	public ResponseEntity<Object> consultar(@PathVariable("idContato") Long idContato) {
 		for (Contato contato : contatos) {
 			if (contato.getId().equals(idContato)) {
 				return ResponseEntity.status(HttpStatus.OK).body(contato);
 			}
 		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não Encontrado...");
+	}
+	
+	@DeleteMapping("/{idContato}")
+	public ResponseEntity<Object> deletar(@PathVariable("idContato") Long idContato) {
+		for (Contato contato : contatos) {
+			if (contato.getId().equals(idContato)) {
+				contatos.remove(contato);
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			}
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contato não Encontrado...");
 	}
 }
